a9612f5b2d7d7ecfcaf4e9c7e078eb215054a9f5
Chet Corey <chetcorey@hotmail.com>
2018-10-09 00:00:13 -0400

    bugfix: now returns ties for longest_word

--- longest_english_word.rb
+++ longest_english_word.rb
@@ -13,11 +13,16 @@ when '.txt'
   words = file.split(/[\r\n]+/)
 end
 
+@longest_words = []
 words_long_to_short = words.sort_by(&:length).reverse
 words_long_to_short.each do |word|
   word_split_into_charters = word.split('')
-  @longest_word = word
-  break word if (word_split_into_charters & excluded_seven_segment_charters).empty?
+  if (word_split_into_charters & excluded_seven_segment_charters).empty?
+    @longest_words_length
+    break word if word.length < @longest_words_length.to_i
+    @longest_words << word
+    @longest_words_length = word.length
+  end
 end
 
-puts @longest_word
+puts @longest_words
